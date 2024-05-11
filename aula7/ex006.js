68216c9b1af2bb8deb18869281f407d773f40d04
Joao Gabriel <joaogabrielantunesoliv@gmail.com>
2024-05-11 00:33:55 -0300

    aula 7 Operadores pt 1

---
+++ aula7/ex006.js
@@ -0,0 +1,27 @@
+//Operadores aritmeticos
+var a = 5 + 3 //soma
+var b = a % 5 //resto de divisao inteira
+var c = 5 * b ** 2 //multiplicacao e potencia
+var d = 10 - a / 2 //subtracao e divisao
+var e = 6 * 2 / d
+var f = b % e + 4 / e
+console.log(a, b, c, d, e, f)
+
+//auto-atribuicoes
+
+var n = 3
+n += 4
+n -= 5
+n *= 4
+n /= 2
+n **= 2
+n %= 5
+
+console.log (n)
+
+//incremento
+
+i = 5
+i++
+
+console.log(i)
