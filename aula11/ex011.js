031b92cd0ffee5961604770e36a392171e9f2e5a
Joao Gabriel <joaogabrielantunesoliv@gmail.com>
2024-05-14 23:05:01 -0300

    aula 11 condicoes pt1

---
+++ aula11/ex011.js
@@ -0,0 +1,6 @@
+var vel = 31
+console.log(`A sua velocidade é de ${vel}km/h`)
+if (vel > 30) {
+    console.log('Você ultrapassou a velocidade permitida!')
+}
+console.log('Utilize cinto de segurança')
---
+++ aula11/ex012.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+}
+else {
+    console.log('Estrangeiro')
+    console.log(`Vivendo em ${pais}`)
+}
