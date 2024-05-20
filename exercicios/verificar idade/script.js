2b753805e6d51f5630ad054e97b43e0c4e3523dd
Joao Gabriel <joaogabrielantunesoliv@gmail.com>
2024-05-20 17:18:56 -0300

    Exercicio verifica idade pt 2

--- exercicios/verificar idade/script.js
+++ exercicios/verificar idade/script.js
@@ -1,9 +1,47 @@
-function calcular() {
+
+function validaidade() {
+    //coleta a idade
     var ano = new Date ()
-    anoatual = ano.getFullYear() 
-    var idade = window.document.getElementById('iidade')
-    idade = Number(idade.value)
-    idade.innerHTML = idade
+    anoatual = ano.getFullYear()
     var mensagem = window.document.getElementById('mostraidade')
-    mensagem.innerHTML = `Detectamos uma pessoa de ${anoatual - idade}`
+    var anoquenasceu = window.document.getElementById('iidade')
+    var idade  = anoatual - Number(anoquenasceu.value)
+    var img = window.document.getElementById('iimagem') 
+    //coleta o sexo
+    const sexOptions = document.getElementsByName('sexo');
+            let selectedSex = null;
+            // Itera sobre os elementos para encontrar o selecionado
+            for (const option of sexOptions) {
+                if (option.checked) {
+                    selectedSex = option.value;
+                    break;
+                }
+            }
+    //imagem 
+    if (idade <= 10 && selectedSex == 'Mulher') {
+        img.src = 'img/criança-F.png'
+    } else if (idade <= 10 && selectedSex == 'Homem') {
+        img.src = 'img/criança-M.png'
+    } else if (idade > 10 && idade < 18 && selectedSex == 'Mulher') {
+        img.src = 'img/adolescente-F.png'
+    } else if (idade > 10 && idade < 18 && selectedSex == 'Homem') {
+        img.src = 'img/adolescente-M.png'
+    } else if (idade >= 18 && idade < 60 && selectedSex == 'Mulher') {
+        img.src = 'img/adulto-F.png'
+    } else if (idade >= 18 && idade < 60 && selectedSex == 'Homem') {
+        img.src = 'img/adulto-M.png'
+    } else if (idade > 60 && selectedSex == 'Mulher') {
+        img.src = 'img/idoso-F.png'
+    } else if (idade > 60 && selectedSex == 'Homem') {
+        img.src = 'img/idoso-M.png'
+    }
+        
+    //verifica falta de dados
+    if (Number(anoquenasceu.value) < 1900 || Number(anoquenasceu.value) > anoatual) {
+        mensagem.innerHTML = 'Digite um ano válido!'
+    }  else if (selectedSex == null){
+        mensagem. innerHTML = 'Por favor, selecione um sexo'            
+    }   else {
+            mensagem.innerHTML = `Detectamos um(a) ${selectedSex} de ${idade} anos`}
 }
+
